48f46f5bac3a77f684dd436c61ee5b58d14fb613
Mehaak N <millimehaak07@gmail.com>
2026-08-29 22:49:47 +0530

    Added rotate array program

---
+++ Week4/RotateArray.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class RotateArray {
+
+    static int[] rotateArray(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        int[] newArray = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            newArray[(i + k) % nums.length] = nums[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        int[] result = rotateArray(nums, k);
+
+        System.out.print("Rotated array: ");
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+
+        sc.close();
+    }
+}
