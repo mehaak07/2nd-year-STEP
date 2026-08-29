b61dc3fb4ab30b6fa58d099ae731905e599ac9e8
Mehaak N <millimehaak07@gmail.com>
2026-08-29 23:05:36 +0530

    Added 3Sum program

---
+++ Week4/ThreeSum.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class ThreeSum {
+
+    static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Sort the array
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result.add(Arrays.asList(
+                            nums[i], nums[left], nums[right]));
+
+                    left++;
+                    right--;
+
+                    // Skip duplicate left values
+                    while (left < right &&
+                           nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right &&
+                           nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+
+                    left++;
+
+                } else {
+
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        List<List<Integer>> result = threeSum(nums);
+
+        System.out.println("Triplets: " + result);
+
+        sc.close();
+    }
+}
